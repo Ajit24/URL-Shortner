e4d3bc7aaa8f6651710499ea11a7c004691def05
Ajit kumar pandey <ajitpandey917@gmail.com>
2022-06-30 23:46:24 +0530

    url database

--- server/config/db.config.js
+++ server/config/db.config.js
@@ -2,7 +2,8 @@
 const mongoose = require('mongoose')
 
 // declare a Database string URI
-const DB_URI = 'mongodb://localhost:27017/urlshortener'
+//const DB_URI = 'mongodb://localhost:27017/urlshortener'
+  const DB_URI = 'mongodb+srv://ajit:12345@cluster0.ihmtj.mongodb.net/url?retryWrites=true&w=majority'
 
 // establishing a database connection
 const connection=()=>{
